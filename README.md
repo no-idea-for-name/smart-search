bfe5d84797ccf50fc32a8903a07ac7175b488d5e
Duck2302 <95743159+Duck2302@users.noreply.github.com>
2024-11-17 14:47:52 +0100

    addes api versioning and rough roadmap for version 0.1.1

--- README.md
+++ README.md
@@ -3,3 +3,13 @@
 ## Setup:
 - go into search-server and run ```docker build -t search-server:0.1.1 .```
 - run ```docker-compose up```
+
+
+
+## Roadmap:
+
+Version 0.1.1:
+- [ ] basic web server to serve search results implemented
+- [ ] serve basic metadata for search results
+- [ ] api documentation and versioning
+- [ ] basic documentation for python flask server
--- search-server/main.py
+++ search-server/main.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, request
 import time
 
 #TODO: find a way to wait for the elasticsearch container to be ready
-time.sleep(15)
+time.sleep(30)
 from document_ingestion import elasticsearch_client, load_pdf_documents
 #Retriever Setup
 retriever = elasticsearch_client.as_retriever(search_kwargs={"score_threshold": 0.7})
@@ -13,7 +13,7 @@ uuids = load_pdf_documents(elasticsearch_client)
 
 app = Flask(__name__)
     
-@app.route("/search/<query>", methods=['GET']) 
+@app.route("/v1/search/<query>", methods=['GET']) 
 def search(query):
     if request.method == 'GET':
         retrieved_documents = retriever.invoke(query)
