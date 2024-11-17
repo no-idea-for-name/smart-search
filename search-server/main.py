cae9cdc01dd776383cc8478e35dbefd4ad64479d
Duck2302 <95743159+Duck2302@users.noreply.github.com>
2024-11-17 20:49:46 +0100

    Changed api to JSON based. Added simple documentation for API usage

--- search-server/main.py
+++ search-server/main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify
 import time
 
 #TODO: find a way to wait for the elasticsearch container to be ready
@@ -13,12 +13,32 @@ uuids = load_pdf_documents(elasticsearch_client)
 
 app = Flask(__name__)
     
-@app.route("/v1/search/<query>", methods=['GET']) 
-def search(query):
-    if request.method == 'GET':
+@app.route("/v1/search", methods=['POST']) 
+def search():
+    if request.method == 'POST':
+        # Parse the JSON request body
+        data = request.get_json()
+        if not data or 'query' not in data:
+            return jsonify({"error": "Missing 'query' in request body"}), 400
+
+        query = data['query']
+        #TODO: Maybe in the future add filters or additional metadata for the search
+        #filters = data.get('filters', {})
+        #options = data.get('options', {})
+
+        # Use the query, filters, and options to retrieve documents
         retrieved_documents = retriever.invoke(query)
 
-        return str(retrieved_documents)
+        # Prepare the JSON response
+        documents = []
+        for document in retrieved_documents:
+            documents.append({
+                "page_content": document.page_content,
+                "metadata": document.metadata
+            })
+
+        # Return the documents as a JSON response
+        return jsonify(documents)
 
 
 
